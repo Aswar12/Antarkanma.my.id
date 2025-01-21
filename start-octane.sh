d0086f1c604f0d2360d7457b1e4127568f887c67
aswar12 <aswarthedoctor@gmail.com>
2025-01-21 15:29:18 +0000

    aswae

--- start-octane.sh
+++ start-octane.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+npm install
+
+npm run build  
+
+
 # Stop any running Octane process
 php artisan octane:stop
 
 # Clear Laravel cache
 php artisan optimize:clear
 
+
+
 # Start Octane with FrankenPHP
-php artisan octane:start --server=frankenphp --host=127.0.0.1 --port=8000 --workers=20 --task-workers=1
+sudo php artisan octane:start --server=frankenphp --host=127.0.0.1 --port=8000 --workers=10 
+
+sudo cp Caddyfile /etc/caddy/Caddyfile && sudo systemctl restart php8.4-fpm && sudo systemctl restart caddy && sudo systemctl status caddy
+
